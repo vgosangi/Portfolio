c0a5cc34d37c68a6c80a11865d02b60acf60b3e1
vgosangi <vgosangi@gmail.com>
2022-11-29 15:52:56 +0530

    dsff

--- js/rock.js
+++ js/rock.js
@@ -133,3 +133,30 @@ paperEl.addEventListener('click', function() {
 rockEL.addEventListener('click', function() {
     playerPick('rock');
 });
+
+
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("rockclose")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.classList.remove('d-none')
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.classList.add('d-none')
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.classList.add('d-none')
+  }
+}
